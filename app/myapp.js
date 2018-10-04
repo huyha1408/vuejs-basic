506b890513423a28cd4e3a8f39f1ee8a016777b9
Huy Ha <huyha14082012@gmail.com>
2018-10-04 12:30:37 +0700

    test template vuejs

--- app/myapp.js
+++ app/myapp.js
@@ -2,7 +2,8 @@ new Vue({
     el : '#app',
     data: {
         title: 'Xin chào',
-        link: 'https://www.facebook.com/sipit.00'
+        link: 'https://www.facebook.com/sipit.00',
+        finishedLink: '<a href="https://www.facebook.com/sipit.00">Huy Hà</a>'
     },
     methods: {
         sayHello: function(){
