068b25d2d10c9e45a880ab25bbd58eb0b379ec0a
Hà Huy <hahuy@Has-iMac.local>
2018-10-11 12:38:25 +0700

    conditional and lists rendering

---
+++ Lists_rendering/v-for.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        parentMessage: 'Parent',
+        items: ['Trà sữa', 'Cà phê', 'Bò húc'],
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Anna', age:'unknown', color: 'blue'}
+        ]
+    },
+});
